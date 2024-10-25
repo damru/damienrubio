7be1d9858cf68aa0459caf4e84ea52a715f3d5d8
Damien Rubio <bonjour@damienrubio.com>
2024-10-25 14:12:04 +0200

    chore: set opacity depending on color scheme and listen to color scheme changes

--- src/assets/js/theme-toggle.js
+++ src/assets/js/theme-toggle.js
@@ -1,13 +1,23 @@
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+let isDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const updateColorScheme = (event) => {
+    isDarkScheme = event.matches;
+};
+const isDarkSchemeListener = window.matchMedia("(prefers-color-scheme: dark)");
+isDarkSchemeListener.addEventListener("change", updateColorScheme);
+
 document.getElementById('theme-toggle').addEventListener('click', function() {
     const body = document.body;
     if (body.classList.contains('forced-light')) {
         body.classList.remove('forced-light');
-        body.classList.add('forced-dark');
+        if (!isDarkScheme) {
+            body.classList.add('forced-dark');
+        }
     } else if (body.classList.contains('forced-dark')) {
         body.classList.remove('forced-dark');
-        body.classList.add('forced-light');
+        if (isDarkScheme) {
+            body.classList.add('forced-light');
+        }
     } else {
-        body.classList.add(prefersDarkScheme ? 'forced-light' : 'forced-dark');
+        body.classList.add(isDarkScheme ? 'forced-light' : 'forced-dark');
     }
 });
