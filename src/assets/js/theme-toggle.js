847ece7b176cf176bce2e7189483abed301948ea
Damien Rubio <bonjour@damienrubio.com>
2024-10-25 15:57:57 +0200

    chore: try improving meta theme-color toggle

--- src/assets/js/theme-toggle.js
+++ src/assets/js/theme-toggle.js
@@ -1,17 +1,26 @@
+function useLightMode(isDarkMode, body) {
+    if (isDarkMode) {
+        body.classList.add('light');
+        document.querySelector('meta[name="theme-color"]').setAttribute("content", "rgb(251,251,251)");
+    }
+}
+
 document.getElementById('theme-toggle').addEventListener('click', function () {
     let isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
     const body = document.body;
+    let themeColor = "rgb(45, 61, 84)";
+    let lightThemeColor = "rgb(196, 215, 242)";
     if (body.classList.contains('light')) {
         body.classList.remove('light');
-        if (!isDarkMode) body.classList.add('dark');
     } else if (body.classList.contains('dark')) {
         body.classList.remove('dark');
-        if (isDarkMode) body.classList.add('light');
+        themeColor = lightThemeColor;
     } else {
-        if (!isDarkMode) {
-            body.classList.add('dark');
-        } else {
+        if (!isDarkMode) body.classList.add('dark');
+        if (isDarkMode) {
             body.classList.add('light');
+            themeColor = lightThemeColor;
         }
     }
+    document.querySelector('meta[name="theme-color"]').setAttribute("content", themeColor);
 });
